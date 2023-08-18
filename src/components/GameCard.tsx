dbeea95ed0e11e7d8b5d26a7de798c24c3cfcb3f
Dichill <dichilltomarong@yahoo.com>
2023-08-17 19:48:25 -0700

    added GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,16 +1,23 @@
-import { Image, Card, CardBody, Heading } from "@chakra-ui/react"
+import { Image, Card, CardBody, Heading, HStack } from "@chakra-ui/react"
 import { Game } from "../services/game-service"
+import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
 
 interface Props {
     game: Game
 }
 
 const GameCard = ({ game } : Props) => {
+    console.log(game)
     return (
         <Card overflow="hidden">
-            <Image src={game.background_image}/>
+            <Image src={game.background_image} objectFit={"cover"} width={'100%'} height={'200px'}/>
             <CardBody>
-                <Heading>{game.name}</Heading>
+                <Heading size={'md'}>{game.name}</Heading>
+                <HStack justifyContent={'space-between'}>
+                    <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)}/>
+                    <CriticScore score={game.metacritic}/>
+                </HStack>
             </CardBody>
         </Card>
     )
--- src/services/game-service.ts
+++ src/services/game-service.ts
@@ -1,10 +1,17 @@
 import create from "../services/http-service";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
-    rating: number;
+    metacritic: number;
+    parent_platforms: { platform: Platform }[]
 }
 
 export interface FetchGameResponse {
