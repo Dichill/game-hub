f65006b8096dede3d0f36a65cce58f51e017ab51
Dichill <dichilltomarong@yahoo.com>
2023-08-19 11:07:56 -0700

    Added Handling of No Image Error

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,15 +1,11 @@
 import errorImage from "../assets/no-image-placeholder.webp";
 
 const getCroppedImageUrl = (url : string) => {
-    try {
-        const target = 'media/'
-        const index = url.indexOf(target) + target.length;
-        return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
-    }
-    catch {
-        return errorImage
-    }
-    
+    if (!url) return errorImage
+
+    const target = 'media/'
+    const index = url.indexOf(target) + target.length;
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
 }
 
 export default getCroppedImageUrl
